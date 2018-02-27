dc898b5318b7aeae4b8bcd462d21a5a3c8b8f50a
Юрий <ddslimsi@gmail.com>
2018-02-27 21:41:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$token = '567179149:AAFwA-kbvtgxusDFVqNoEEhJF_gRUkdcEoo';
+$user_id = $data['message']['chat']['id'];
+
+     $url = 'https://api.telegram.org/bot'.$token.'/sendDocument?chat_id='.$user_id.'&document='.$gif.'.gif';
