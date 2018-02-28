a192f832e13b69d27fd638ca6fc926595726b5cf
Юрий <ddslimsi@gmail.com>
2018-02-28 21:12:30 +0200

    first commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 $token = '567179149:AAFwA-kbvtgxusDFVqNoEEhJF_gRUkdcEoo';
 
 file_get_contents('http://vk-sender.tk/telega.php?param='.serialize($data));
-
+file_put_contents('telega.txt',print_r($data,true));
 
 /*
 $user_id = $data['message']['chat']['id'];
