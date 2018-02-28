988055b8d3d7307c8133bc0d1b852baceacc7ee4
Юрий <ddslimsi@gmail.com>
2018-02-28 21:17:22 +0200

    first commit

--- telegram-bot-call-back.php
+++ telegram-bot-call-back.php
@@ -2,8 +2,18 @@
 $data = json_decode(file_get_contents('php://input'), true);
 $token = '567179149:AAFwA-kbvtgxusDFVqNoEEhJF_gRUkdcEoo';
 
-file_get_contents('http://vk-sender.tk/telega.php?param='.serialize($data));
-file_put_contents('telega.txt',print_r($data,true));
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://vk-sender.tk/telega.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
 
 /*
 $user_id = $data['message']['chat']['id'];
