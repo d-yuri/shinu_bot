3d7c85445b79ada2d8dfd73db3826f7be9a432e2
d-yuri <d-yuri@users.noreply.github.com>
2018-03-08 14:22:42 +0200

    Update telegram-bot-call-back.php

--- telegram-bot-call-back.php
+++ telegram-bot-call-back.php
@@ -1,11 +1,11 @@
 <?php
 $data = json_decode(file_get_contents('php://input'), true);
-$token = '504136360:AAE0LwahwnnNBUZ6lqd-CvJFdEjiaLze2ls';
+$token = '504136360:AAE0LwahwnnNBUZ6lqdls';
 
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"http://vk-send.tk/telegram-shinu/telega.php");
+curl_setopt($ch, CURLOPT_URL,"http:///telegram-shinu/telega.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
 
